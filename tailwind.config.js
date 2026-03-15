96b07c50d04e1b17dc368a2567e8a20f922d7021
sadaf-klinkode <sadaf@klinkode.net>
2026-03-15 10:03:25 +0600

    added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
     	extend: {
     		fontFamily: {
     			sans: [
-    				'Figtree',
+    				'"Public Sans"',
                     ...defaultTheme.fontFamily.sans
                 ]
     		},
